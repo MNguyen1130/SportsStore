03c839b470cac8f7855a926ada8ce945c75d411f
MNguyen1130 <matthewpnguyen@gmail.com>
2015-01-07 12:00:15 -0500

    Entity Framework Context and Product Repository
    Installed Entity Framework Packages to Domain and WebUI Projects. Added
    DB connection to WebConfig. Created Product Repository and changed
    Binding in NinjectDependencyResolver to a Real Repository.

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -34,15 +35,18 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // Put bindings here
-            // Mock Repository
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            });
+            // Real Repository Binding
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
 
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            //// Mock Repository
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product> {
+            //    new Product { Name = "Football", Price = 25 },
+            //    new Product { Name = "Surf board", Price = 179 },
+            //    new Product { Name = "Running shoes", Price = 95 }
+            //});
+
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
