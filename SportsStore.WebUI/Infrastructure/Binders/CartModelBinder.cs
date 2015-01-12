7ed0c32f389403b3ec2c240487abf378570760a2
MNguyen1130 <matthewpnguyen@gmail.com>
2015-01-12 11:29:54 -0500

    Model Binding
    Created Custom Model Binder for Cart, added Unit Test cases for new
    implementation

---
+++ SportsStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,32 @@
+﻿using SportsStore.Domain.Entities;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // Get the Cart from the session
+            Cart cart = null;
+            if (controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+            // Create Cart if there wasn't one in session data
+            if (cart == null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+
+            // Return the cart
+            return cart;
+
+        }
+    }
+}
