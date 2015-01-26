7610c80a83c9822ffb1b6b7a6933fd890bb4d3fe
MNguyen1130 <matthewpnguyen@gmail.com>
2015-01-26 16:03:44 -0500

    Chapter 11 - Administration
    Created Administration side. Allows someone to add products, edit
    products, and delete products.

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,11 +1,24 @@
-﻿namespace SportsStore.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+
+        [Required (ErrorMessage = "Enter a product name")]
         public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required (ErrorMessage = "Enter a description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+
+        [Required (ErrorMessage = "Specify a category")]
         public string Category { get; set; }
     }
 }
